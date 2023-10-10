c9b0e76ce2cd0abaaf9b71a3e6e1c44c8d210412
Matthijs <matthijsslobbe@gmail.com>
2023-10-10 11:16:41 +0200

    added build_goal_constraint_table

--- prioritized.py
+++ prioritized.py
@@ -30,11 +30,10 @@ class PrioritizedPlanningSolver(object):
         result = []
         #constraints = []
         atgoal = {}
-        constraints = [{'agent': 0,
-                        'loc': [(1, 5)],
-                        'timestep': 4}]
+        constraints = [{'agent': 0, 'loc': [(1, 5)], 'timestep': 4},
+                       {'agent': 1, 'loc': [(1, 2), (1, 3)], 'timestep': 1}
+                       ]
         # constraints.append({'agent': 0, 'loc': [(1, 2)], 'timestep': 5}) # Test constraint to see whether the constrained_table is formatted correctly.
-        #constraints.append({'agent': 1, 'loc': [(1,2), (1,3)], 'timestep': 1})
         for i in range(self.num_of_agents):  # Find path for each agent
 
             path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i],
