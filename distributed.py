4ce84aca814c1a01f590a65349ccb0d23632bc7b
stellawessels <stella@wessels.nl>
2023-11-14 20:18:07 +0100

    Distributed last version Stella

--- distributed.py
+++ distributed.py
@@ -1,62 +1,140 @@
-
 import time as timer
 from single_agent_planner import a_star, compute_heuristics
-import random
 
-class DistributedPlanningSolver(object):
+#probeersel - kijken of de agent naar open locaties wilt verplaatsen uit de weg voor de ander - bijvoorbeeld nodig voor edgecollision.txt
+"""
+def find_fallback_location(self, current_location, next_location, occupied_locations):
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 4 directions: up, down, right, left
+    for dx, dy in directions:
+        fallback_location = (current_location[0] + dx, current_location[1] + dy)
+        if fallback_location not in occupied_locations and self.is_location_free(
+                fallback_location) and fallback_location != next_location:
+            return fallback_location
+    return None
+
+
+def is_location_free(self, location):
+    rows, cols = len(self.my_map), len(self.my_map[0])
+    if 0 <= location[0] < rows and 0 <= location[1] < cols:
+        return self.my_map[location[0]][location[1]] == '.'
+    return False
+
+"""
+
+
+class DistributedPlanningSolver:
     def __init__(self, my_map, starts, goals):
         self.my_map = my_map
         self.starts = starts
         self.goals = goals
         self.num_of_agents = len(goals)
         self.CPU_time = 0
-        self.heuristics = [compute_heuristics(my_map, goal) for goal in goals]
+        self.heuristics = [compute_heuristics(my_map, goal) for goal in self.goals]
 
     def find_solution(self):
         start_time = timer.time()
-        paths = [a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i], i, [], self.goals, []) for i in range(self.num_of_agents)]
-        goal_reached_times = [None] * self.num_of_agents
+        paths = []
+        path_lengths = []
         constraints = []
 
-        def remaining_path_length(path, goal):
-            if goal in path:
-                return len(path) - path.index(goal)
-            return 0
+        for i in range(self.num_of_agents):
+            path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i], i, constraints, self.goals,
+                          path_lengths)
+            paths.append(path)
+            path_lengths.append(len(path) if path else 0)
 
-        while not all(goal_reached_times):
-            timestep_location_list = []
-            for agentnumber, agentpath in enumerate(paths):
-                for timestep_agentpath, location_agentpath in enumerate(agentpath):
-                    timestep_location_list.append({'agent': agentnumber, 'loc': location_agentpath, 'timestep': timestep_agentpath})
+        max_timesteps = max(len(path) for path in paths)
+        timestep = 0
+        print(paths)
 
-            conflicts_list = []
-            for position1 in timestep_location_list:
-                for position2 in timestep_location_list:
-                    if position1['loc'] == position2['loc'] and position1['timestep'] == position2['timestep'] and position1 != position2:
-                        conflicts_list.append({'agent1': position1['agent'], 'agent2': position2['agent'], 'loc': position1['loc'], 'timestep': position1['timestep']})
+        while timestep < max_timesteps:
+            has_conflicts = True
 
-            if conflicts_list:
-                # Resolve conflicts
-                for conflict in conflicts_list:
-                    agent1 = conflict['agent1']
-                    agent2 = conflict['agent2']
-                    agent_to_adjust = agent1 if remaining_path_length(paths[agent1], self.goals[agent1]) < remaining_path_length(paths[agent2], self.goals[agent2]) else agent2
+            while has_conflicts:
+                timestep_location_list = []
+                for agentnumber, agentpath in enumerate(paths):
+                    for timestep_agentpath, location_agentpath in enumerate(agentpath):
+                        timestep_location_list.append(
+                            {'agent': agentnumber, 'loc': location_agentpath, 'timestep': timestep_agentpath})
+                # print(timestep_location_list)
 
-                    conflict_timestep = conflict['timestep']
-                    new_start = paths[agent_to_adjust][conflict_timestep - 1] if conflict_timestep > 0 else self.starts[agent_to_adjust]
-                    constraints.append({'agent': agent_to_adjust, 'loc': [conflict['loc']], 'timestep': conflict_timestep})
-                    new_path_segment = a_star(self.my_map, new_start, self.goals[agent_to_adjust], self.heuristics[agent_to_adjust], agent_to_adjust, constraints, self.goals, [])
+                conflicts = []
+                for position1 in timestep_location_list:
+                    for position2 in timestep_location_list:
+                        if position1['agent'] < position2['agent'] and position1['timestep'] == position2['timestep']:
+                            if position1['loc'] == position2['loc']:
+                                conflicts.append((position1, position2, 'vertex'))
+                            elif position1['timestep'] > 0 and position2['timestep'] > 0:
+                                prev_loc1 = paths[position1['agent']][position1['timestep'] - 1]
+                                prev_loc2 = paths[position2['agent']][position2['timestep'] - 1]
+                                if prev_loc1 == position2['loc'] and prev_loc2 == position1['loc']:
+                                    conflicts.append((position1, position2, 'edge'))
 
-                    if new_path_segment:
-                        paths[agent_to_adjust] = paths[agent_to_adjust][:conflict_timestep] + new_path_segment[1:]
+                if conflicts:
+                    # Find the lowest timestep with a conflict
+                    lowest_timestep_conflicts = [c for c in conflicts if
+                                                 c[0]['timestep'] == min(c[0]['timestep'] for c in conflicts)]
+                    # print(lowest_timestep_conflicts)
 
-            # Update agents' status
-            for i, path in enumerate(paths):
-                if goal_reached_times[i] is None and path[-1] == self.goals[i]:
-                    goal_reached_times[i] = len(path)
+                    # Resolve only conflicts at the lowest timestep
+                    for conflict in lowest_timestep_conflicts:
+                        print(conflict)
+                        agent1, agent2, constrainttype = conflict
 
-        average_time_to_goal = sum(time for time in goal_reached_times if time is not None) / self.num_of_agents
-        print(f"Average Time for Agents to Reach Goals: {average_time_to_goal:.2f} timesteps")
+                        l1 = 0
+                        for step in paths[agent1['agent']]:
+                            # print(step)
+                            # print(agent1['loc'])
+                            l1 += 1
+                            if step == agent1['loc']:
+                                # print('gevonden')
+                                break
+                        # print(path_lengths[agent1['agent']])
+                        remaining_length_1 = path_lengths[agent1['agent']] - l1
+                        # print('remaining_length',remaining_length_1)
+                        l2 = 0
+                        for step in paths[agent2['agent']]:
+                            l2 += 1
+                            if step == agent2['loc']:
+                                break
+                        remaining_length_2 = path_lengths[agent2['agent']] - l2
+                        # print(remaining_length_1)
+                        # print(remaining_length_2)
+                        if remaining_length_1 > remaining_length_2:
+                            agent_to_adjust = agent2['agent']
+                        else:
+                            agent_to_adjust = agent1['agent']
+                        # print('agent_to_adjust',agent_to_adjust)
 
-        self.CPU_time = timer.time() - start_time
-        return paths
+                        # update constraints
+                        agent1, agent2, constraint_type = conflict
+                        constraint = {'agent': agent_to_adjust, 'timestep': agent2['timestep']}
+                        if constraint_type == 'vertex':
+                            constraint['loc'] = [agent1['loc']]
+                        else:  # edge conflict
+                            constraint['loc'] = [agent1['loc'], agent2['loc']]
+
+                        constraints.append(constraint)
+                        print('constraints_list', constraints)
+
+                        new_start = paths[agent_to_adjust][agent1['timestep']-1 ] if agent1['timestep'] > 0 else \
+                        self.starts[agent_to_adjust]
+                        new_path = a_star(self.my_map, new_start, self.goals[agent_to_adjust],
+                                          self.heuristics[agent_to_adjust], agent_to_adjust, constraints, self.goals,
+                                          path_lengths)
+                        # print(new_path)
+                        if new_path is None:
+                            return
+                        paths[agent_to_adjust] = paths[agent_to_adjust][:agent1['timestep']] + new_path
+                        path_lengths[agent_to_adjust] = len(paths[agent_to_adjust])
+                        # print(paths[agent_to_adjust])
+
+                    # Update max_timesteps in case paths have been extended
+                max_timesteps = max(len(path) for path in paths)
+
+                for i, path in enumerate(paths):
+                    if len(path) == max_timesteps:
+                        self.CPU_time = timer.time() - start_time
+                        return paths
+
+                timestep += 1
